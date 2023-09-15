6afbaaab60aea0e4c431da4fc7d53c234d7cc568
senasli <sen.asli@student.atilim.edu.tr>
2023-09-15 22:45:59 +0300

    encapsulation example

---
+++ Encapsulationkavrami/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Encapsulationkavrami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Student student = new Student();
+        student.Name="Asli";
+        student.Lastname = "Sen";
+        student.No = 293;
+        student.Classs = 3;
+
+        student.passtheclass();
+        student.GetStudentInfo();
+
+        Student student2 = new Student();
+        student2.Name="Deniz";
+        student2.Lastname = "öz";
+        student2.No = 345;
+        student2.Classs = 1;
+
+        student2.failtheclass();
+        student2.failtheclass();
+        student2.GetStudentInfo();
+
+        }
+    }
+
+     class Student
+    {
+        private string name;
+        private string lastname;
+        private int no;
+        private int classs;
+
+     public string Name{get => name; set => name = value; }
+     public string Lastname{get => lastname; set => lastname = value; }
+     public int No{get => no; set => no = value; }
+     public int Classs{get => classs; 
+     set {
+        if(value<1){
+            Console.WriteLine("Class must be at least 1!");
+            classs = 1;
+            }
+        else 
+        classs = value;
+     }
+      }
+
+  public Student(string name, string lastname, int no, int classs)
+  { 
+    Name= name;
+    Lastname= lastname;
+    No= no;
+    Classs= classs;
+  
+  }
+
+  public Student(){ }
+
+public void GetStudentInfo()
+        {
+              Console.WriteLine("****************student's info******************");
+            Console.WriteLine("Name of the student: {0}",this.Name);
+            Console.WriteLine("Last Name of the student: {0}",this.Lastname);
+            Console.WriteLine("Id of the student: {0}",this.No);
+            Console.WriteLine("Class of the student: {0}",this.Classs);
+        }
+
+public void passtheclass(){
+this.Classs = this.Classs+1;
+}
+
+public void failtheclass(){
+this.Classs = this.Classs -1;
+}
+    }
+}
